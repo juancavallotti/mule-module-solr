165fb3be61444f5d292d77c26948b5790fce12bb
Juan Alberto Lopez Cavallotti <juancavallotti@gmail.com>
2013-03-10 19:26:51 -0300

    Added site

---
+++ assets/navtree_data.js
@@ -0,0 +1,18 @@
+var NAVTREE_DATA =
+[ [ "org.apache.solr.client.solrj.transformers", "org/apache/solr/client/solrj/transformers/package-summary.html", [ [ "Classes", null, [ [ "ORDEREnumTransformer", "org/apache/solr/client/solrj/transformers/ORDEREnumTransformer.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules", "org/mule/modules/package-summary.html", [ [ "Classes", null, [ [ "SolrConnector", "org/mule/modules/SolrConnector.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "SolrModuleException", "org/mule/modules/SolrModuleException.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.adapters", "org/mule/modules/adapters/package-summary.html", [ [ "Classes", null, [ [ "SolrConnectorCapabilitiesAdapter", "org/mule/modules/adapters/SolrConnectorCapabilitiesAdapter.html", null, "" ], [ "SolrConnectorConnectionManager", "org/mule/modules/adapters/SolrConnectorConnectionManager.html", null, "" ], [ "SolrConnectorConnectionManager.ConnectionKey", "org/mule/modules/adapters/SolrConnectorConnectionManager.ConnectionKey.html", null, "" ], [ "SolrConnectorLifecycleAdapter", "org/mule/modules/adapters/SolrConnectorLifecycleAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.config", "org/mule/modules/config/package-summary.html", [ [ "Classes", null, [ [ "DeleteByIdDefinitionParser", "org/mule/modules/config/DeleteByIdDefinitionParser.html", null, "" ], [ "DeleteByQueryDefinitionParser", "org/mule/modules/config/DeleteByQueryDefinitionParser.html", null, "" ], [ "IndexDefinitionParser", "org/mule/modules/config/IndexDefinitionParser.html", null, "" ], [ "IndexPojoDefinitionParser", "org/mule/modules/config/IndexPojoDefinitionParser.html", null, "" ], [ "QueryDefinitionParser", "org/mule/modules/config/QueryDefinitionParser.html", null, "" ], [ "SolrConnectorConfigDefinitionParser", "org/mule/modules/config/SolrConnectorConfigDefinitionParser.html", null, "" ], [ "SolrConnectorNamespaceHandler", "org/mule/modules/config/SolrConnectorNamespaceHandler.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.processors", "org/mule/modules/processors/package-summary.html", [ [ "Classes", null, [ [ "DeleteByIdMessageProcessor", "org/mule/modules/processors/DeleteByIdMessageProcessor.html", null, "" ], [ "DeleteByQueryMessageProcessor", "org/mule/modules/processors/DeleteByQueryMessageProcessor.html", null, "" ], [ "IndexMessageProcessor", "org/mule/modules/processors/IndexMessageProcessor.html", null, "" ], [ "IndexPojoMessageProcessor", "org/mule/modules/processors/IndexPojoMessageProcessor.html", null, "" ], [ "QueryMessageProcessor", "org/mule/modules/processors/QueryMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.modules.transformers", "org/mule/modules/transformers/package-summary.html", [ [ "Classes", null, [ [ "MessageToInputDocumentTransformerTransformer", "org/mule/modules/transformers/MessageToInputDocumentTransformerTransformer.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
